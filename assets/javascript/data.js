384c044215206e3ae109ec75edf13724459fd2da
brian wisnowski <brian wisnowski>
2017-04-09 15:50:43 -0400

    all done-- with comments

--- assets/javascript/data.js
+++ assets/javascript/data.js
@@ -1,3 +1,5 @@
+//initializes the firebase database
+
 var config = {
     apiKey: "AIzaSyDzhidhdMdDHZihXGxCYfU0Gtt9XeWBDR0",
     authDomain: "awesome-airplanes.firebaseapp.com",
@@ -9,10 +11,9 @@ var config = {
 
   firebase.initializeApp(config);
 
-
-
-
 var database = firebase.database();
+
+// below on click of the add airline button the inputs will add what the user puts in the text fields into a data set child. 
 
 $("#add-airline-btn").on("click", function(event) {
   event.preventDefault();
@@ -23,7 +24,7 @@ var firstPlane = $("#first-input").val().trim();
 var tfrequency = $("#frequency-input").val().trim();
 
 
-
+//creates local object for holding data
 
 var newAirline = {
   name: airlineName,
@@ -32,15 +33,17 @@ var newAirline = {
   frequency: tfrequency
 };
 
+
+// uploads data to my firebase "Awesome Airplanes"
 database.ref().push(newAirline);
 
-console.log(newAirline.name);
-console.log(newAirline.destination)
 
+//clears form
 $("#airline-input").val("");
 $("#destination-input").val("");
  $("#first-input").val("");
  $("#frequency-input").val("");
+
 
 return false;
 
@@ -48,10 +51,12 @@ return false;
 
 
 
-
+// functions for adding a new plane into the table
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
 console.log(childSnapshot.val());
+
+
 
 var airlineName = childSnapshot.val().name;
 var destName = childSnapshot.val().destination;
@@ -60,26 +65,34 @@ var tfrequency = childSnapshot.val().frequency;
 
 
 
+//use the moment jquery to convert military to standard time
 
+//create the inital time by entering it into the firstPlane input of the form and push back a year to ensure it comes before current time (in case dates are messed up maybe?)
 var firstPlaneConverted = moment(firstPlane, "hh:mm").subtract(1, "years");
 console.log(firstPlaneConverted);
 
 var currentTime = moment();
-console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
+//difference bettween times
 var diffTime = moment().diff(moment(firstPlaneConverted), "minutes");
 
+//getting the remainder
 var tRemainder = diffTime % tfrequency;
 
+// figuring out next arrival of plan
 var tMinutesTillPlane = tfrequency - tRemainder;
 
+//next airplane added
+
 var nextPlane = moment().add(tMinutesTillPlane, "minutes");
-console.log("Arrival Time: " + moment(nextPlane).format("hh:mm"));
+
 
 
 
 $("#schedule-table > tbody").append("<tr><td>" + airlineName + "</td><td>" + destName + "</td><td>"+ tfrequency +  "</td><td>" + nextPlane.format("hh:mm") + "</td><td>" + tMinutesTillPlane + "</td></tr>");
 
+//add the inputs and calculations into a table through a train 
+//add the formt for the "hh:mm", otherwise is goes to the seconds since some random date.
 
 
 
